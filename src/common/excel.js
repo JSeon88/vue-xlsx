921c14a706d96b39e3ae3bc34c5942070331966d
odini <odini@nhnsoft.com>
2022-03-03 11:48:20 +0900

    .

--- src/common/excel.js
+++ src/common/excel.js
@@ -39,47 +39,91 @@ export const fileDownload = (blob, filename) => {
 };
 
 /**
- * 엑셀 파일 생성
- * @param {{text: string; style: {[key: string]: any}}[][] | string[][]} rawData
- * @param {string} filename
+ *
+ * @param {*} rows
+ * @param {*} sheet
  */
-export async function createExcelFile(rawData, filename) {
-  const workbook = await XlsxPopulate.fromBlankAsync();
-  // 워크북 생성
-  const sheet = workbook.sheet("Sheet1");
-  // row 순회
-  rawData.forEach((rows, rowIndex) => {
+function insertCellData(rows, sheet, startRowIndex = 0) {
+  rows.forEach((columns, rowIndex) => {
     // column 순회
-    rows.forEach((column, columnIndex) => {
+    columns.forEach((column, columnIndex) => {
       // 셀 삽입 데이터 변수 생성
       let insertData = null;
       // 컬럼 데이터가 배열 인지 여부(이 경우에는 richText 입력으로 간주)
-      if (Array.isArray(column)) {
+      if (Array.isArray(column?.text)) {
         const richText = new RichText();
         // richText 의 스타일 속성 적용
-        column.forEach((richData) => {
-          richText.add(richData.text, richData.style);
+        column.text.forEach(({ text, style }) => {
+          console.log({ text, style });
+          richText.add(text, (style = style || {}));
         });
         // 셀 삽입 변수 변경
         insertData = richText;
-      } else if (typeof column === "string") {
+      } else if (typeof column?.text === "string") {
         // 이 경우는 셀 삽입 변수가 문자열, 셀 삽입 변수 변경
-        insertData = column;
+        insertData = column.text;
       }
 
       // 셀 삽입 변수가 존재하는 경우에만 삽입
       if (insertData) {
         // 알파벳 좌표 계산
-        const positionName = `${getColumnAlphabet(columnIndex)}${rowIndex + 1}`;
+        const positionName = `${getColumnAlphabet(columnIndex)}${
+          rowIndex + startRowIndex + 1
+        }`;
         // 셀 생성
         const cell = sheet.cell(positionName);
         cell.value(insertData);
+
+        if (column?.style) {
+          Object.keys(column.style).forEach((key) => {
+            console.log(positionName, key, column.style[key]);
+            cell.style(key, column.style[key]);
+          });
+        }
       }
     });
+  });
+}
+
+/**
+ * 엑셀 파일 생성
+ * @param {*} rawData
+ * @param {string} filename
+ */
+export async function createExcelFile(
+  { columnWidth, merges, category, list },
+  filename
+) {
+  const workbook = await XlsxPopulate.fromBlankAsync();
+  // 워크북 생성
+  const sheet = workbook.sheet("Sheet1");
+
+  // 카테고리 셀 데이터 주입
+  insertCellData(category, sheet, 0);
+
+  // 목록 셀 데이터 주입
+  insertCellData(list, sheet, category.length);
+
+  // 컬럼 넓이 지정
+  columnWidth.forEach((width, columnIndex) => {
+    console.log("컬럼 넓이지정", getColumnAlphabet(columnIndex), width);
+    sheet.column(getColumnAlphabet(columnIndex)).width(width);
+  });
+
+  // 셀 머지
+  merges.forEach(({ start, end }) => {
+    const startPositionName = `${getColumnAlphabet(start.columnIndex)}${
+      start.rowIndex + 1
+    }`;
+    const endPositionName = `${getColumnAlphabet(end.columnIndex)}${
+      end.rowIndex + 1
+    }`;
+    sheet.range(`${startPositionName}:${endPositionName}`).merged(true);
   });
 
   // blob 생성
   const blob = await workbook.outputAsync();
+
   // 파일 다운로드
   fileDownload(blob, filename);
 }
--- src/components/ExcelDownload.vue
+++ src/components/ExcelDownload.vue
@@ -11,58 +11,373 @@ export default {
   name: "ExcelDownload",
   setup() {
     // 서버에서 추가정보 리스트 가져오기
-    const rawData = [
-      [
+    const data = {
+      // 컬럼 넓이(단위 : 노출 글자 수)
+      columnWidth: [20, 25, 20, 20, 30, 30, 30, 30],
+      merges: [
+        {
+          start: { rowIndex: 0, columnIndex: 0 },
+          end: { rowIndex: 0, columnIndex: 2 },
+        },
+      ],
+      category: [
         [
           {
-            text: "a1-1\n",
-            style: { italic: true, bold: true },
-          },
-          {
-            text: "a1-2",
-            style: { fontColor: "FF0000" },
+            text: [
+              {
+                text: "응시자 일괄 등록 엑셀 서식",
+                style: { bold: true, fontSize: 24 },
+              },
+            ],
           },
         ],
-        null,
         [
           {
-            text: "c1-1\n",
-            style: { italic: true, bold: true },
+            style: {
+              border: true,
+              verticalAlignment: "top",
+            },
+            text: [
+              {
+                text: "설명",
+                style: { bold: true },
+              },
+            ],
           },
           {
-            text: "c1-2",
-            style: { fontColor: "FF0000" },
+            style: {
+              border: true,
+              verticalAlignment: "top",
+            },
+            text: [
+              {
+                text: "(선택)\n",
+              },
+              {
+                text: "응시자가 속할 그룹명을 입력해주세요.",
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              verticalAlignment: "top",
+            },
+            text: [
+              {
+                text: "(필수)\n",
+                style: { fontColor: "FF0000" },
+              },
+              {
+                text: "응시자 이름을 입력해주세요.",
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              verticalAlignment: "top",
+            },
+            text: [
+              {
+                text: "(필수)\n",
+                style: { fontColor: "FF0000" },
+              },
+              {
+                text: "시험 응시 로그인시 사용할 비밀번호를 입력해주세요.",
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              verticalAlignment: "top",
+            },
+            text: [
+              {
+                text: "(선택)\n",
+              },
+              {
+                text: "응시자 수집정보 설정에 등록할 내용을 입력해주세요.",
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              verticalAlignment: "top",
+            },
+            text: [
+              {
+                text: "(선택)\n",
+              },
+              {
+                text: "응시자 수집정보 설정에 등록할 내용을 입력해주세요.",
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              verticalAlignment: "top",
+            },
+            text: [
+              {
+                text: "(선택)\n",
+              },
+              {
+                text: "응시자 수집정보 설정에 등록할 내용을 입력해주세요.",
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              verticalAlignment: "top",
+            },
+            text: [
+              {
+                text: "(선택)\n",
+              },
+              {
+                text: "응시자 수집정보 설정에 등록할 내용을 입력해주세요.",
+              },
+            ],
+          },
+        ],
+        [
+          {
+            style: {
+              border: true,
+              verticalAlignment: "top",
+            },
+            text: [
+              {
+                text: "예시",
+                style: { bold: true },
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              verticalAlignment: "top",
+            },
+            text: [
+              {
+                text: "아이엠수학학원 3학년 심화반",
+                style: { fontColor: "666666" },
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              verticalAlignment: "top",
+            },
+            text: [
+              {
+                text: "홍길동",
+                style: { fontColor: "666666" },
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              verticalAlignment: "top",
+            },
+            text: [
+              {
+                text: "math03010",
+                style: { fontColor: "666666" },
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              verticalAlignment: "top",
+            },
+            text: [
+              {
+                text: "예)추가정보1 : 휴대전화번호\n010-1234-5678",
+                style: { fontColor: "666666" },
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              verticalAlignment: "top",
+            },
+            text: [
+              {
+                text: "예) 추가정보2 : 학교\n아이엠초등학교",
+                style: { fontColor: "666666" },
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              verticalAlignment: "top",
+            },
+            text: [
+              {
+                text: "예) 추가정보3 : 학년\n3학년",
+                style: { fontColor: "666666" },
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              verticalAlignment: "top",
+            },
+            text: [
+              {
+                text: "예) 추가정보4 : 레벨테스트\nlv.4",
+                style: { fontColor: "666666" },
+              },
+            ],
+          },
+        ],
+        [
+          {
+            style: {
+              border: true,
+              fill: "C6C6C6",
+            },
+            text: " ",
+          },
+          {
+            style: {
+              border: true,
+              fill: "C6C6C6",
+            },
+            text: [
+              {
+                text: "그룹",
+                style: { bold: true },
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              fill: "C6C6C6",
+            },
+            text: [
+              {
+                text: "이름 ",
+                style: { bold: true },
+              },
+              {
+                text: "*",
+                style: { fontColor: "FF0000" },
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              fill: "C6C6C6",
+            },
+            text: [
+              {
+                text: "비밀번호 ",
+                style: { bold: true },
+              },
+              {
+                text: "*",
+                style: { fontColor: "FF0000" },
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              fill: "C6C6C6",
+            },
+            text: [
+              {
+                text: "추가정보1",
+                style: { bold: true },
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              fill: "C6C6C6",
+            },
+            text: [
+              {
+                text: "추가정보2",
+                style: { bold: true },
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              fill: "C6C6C6",
+            },
+            text: [
+              {
+                text: "추가정보3",
+                style: { bold: true },
+              },
+            ],
+          },
+          {
+            style: {
+              border: true,
+              fill: "C6C6C6",
+            },
+            text: [
+              {
+                text: "추가정보4",
+                style: { bold: true },
+              },
+            ],
           },
         ],
       ],
-      [
+      list: [
         [
           {
-            text: "a2-1\n",
-            style: { italic: true, bold: true },
+            text: " ",
           },
           {
-            text: "a2-2",
-            style: { fontColor: "FF0000" },
-          },
-        ],
-        "b2",
-        [
-          {
-            text: "c2-1\n",
-            style: { italic: true, bold: true },
+            text: "EDu",
           },
           {
-            text: "c2-2",
-            style: { fontColor: "FF0000" },
+            text: "Sunny",
+          },
+          {
+            text: "1234",
+          },
+          {
+            text: "추가정보 111",
+          },
+          {
+            text: "추가정보 222",
+          },
+          {
+            text: "추가정보 333",
+          },
+          {
+            text: "추가정보 444",
           },
         ],
       ],
-    ];
+    };
 
     function excelDownFile() {
       console.log("down...");
-      createExcelFile(rawData, "테스트-다운로드.xlsx");
+      createExcelFile(data, "테스트-다운로드.xlsx");
     }
 
     return {
